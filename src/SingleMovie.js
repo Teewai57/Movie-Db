3fac462e1a0354b16d0e43c5563328d6c2c23d3c
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-04 12:46:31 +0100

    returned loader and error msg

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -7,6 +7,21 @@ import useFetch from './useFetch'
 const SingleMovie = () => {
     const { id } = useParams()
     const { isLoading, error, data:movie } = useFetch(`&i=${id}`)
+
+
+    if (isLoading) {
+        return <div className='loading'></div>
+      }
+      if (error.show) {
+        return (
+          <div className='page-error'>
+            <h1>{error.msg}</h1>
+            <Link to='/' className='btn'>
+              back to movies
+            </Link>
+          </div>
+        )
+      }
   return <h2>single movie</h2>
 }
 
