0627536d58177135ebd857be89a820dcedc2fe88
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-04 12:36:57 +0100

    mapped movies

--- src/Movies.js
+++ src/Movies.js
@@ -13,7 +13,25 @@ const Movies = () => {
     }
 
 
-  return <h2>movies component</h2>
+  return (
+    <section className='movies'>
+      {movies.map((movie) => {
+        const { imdbID: id, Poster: poster, Title: title, Year: year } = movie
+
+        return (
+          <Link to={`/movies/${id}`} key={id} className='movie'>
+            <article>
+              <img src={poster === 'N/A' ? url : poster} alt={title} />
+              <div className='movie-info'>
+                <h4 className='title'>{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        )
+      })}
+    </section>
+  )
 }
 
 export default Movies
