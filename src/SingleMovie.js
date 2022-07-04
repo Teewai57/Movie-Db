fc3f11bb07639a96b8e0be0cdcf510f9d2e87c8c
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-04 12:41:53 +0100

    made API call

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { API_ENDPOINT } from './context'
+import useFetch from './useFetch'
+
 
 const SingleMovie = () => {
+    const { id } = useParams()
+    const { isLoading, error, data:movie } = useFetch(`&i=${id}`)
   return <h2>single movie</h2>
 }
 
