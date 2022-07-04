a28463f3d8f7fe8d5a4176c0d34475ebf2ffa2f7
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-04 12:48:50 +0100

    returned poster and single movie detail

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -22,7 +22,21 @@ const SingleMovie = () => {
           </div>
         )
       }
-  return <h2>single movie</h2>
-}
+
+      const { Poster: poster, Title: title, Plot: plot, Year: year } = movie
+
+      return (
+        <section className='single-movie'>
+          <img src={poster} alt={title} />
+          <div className='single-movie-info'>
+            <h2>{title}</h2>
+            <p>{plot}</p>
+            <h4>{year}</h4>
+            <Link to='/' className='btn'>
+              back to movies
+            </Link>
+          </div>
+        </section>
+      )}
 
 export default SingleMovie
