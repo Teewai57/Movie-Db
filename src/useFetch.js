45921eaa2304dbdcc97724e3afab2cf51e962f75
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-05 22:30:28 +0100

    changed api key

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+const API_ENDPOINT = "http://www.omdbapi.com/?i=tt3896198&apikey=8d7afd2"
 
 const useFetch = (urlParams) => {
   const [isLoading, setIsLoading] = useState(true)
